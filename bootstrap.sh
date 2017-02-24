f578d8d60e0702ef18caed4ce8ceaac682e13f02
Erick Tryzelaar <erick.tryzelaar@gmail.com>
2017-02-23 19:44:59 -0800

    Add a database bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -eu
+
+cd `dirname $0`
+
+# Install diesel CLI if they're not installed.
+if ! command -v diesel >/dev/null 2>&1; then
+	echo Installing the diesel CLI
+	cargo install diesel_cli
+fi
+
+echo Running the migrations...
+
+# Create the database.
+diesel migration run \
+	--database-url db.sqlite \
+	--migration-dir quad-diesel/migrations
